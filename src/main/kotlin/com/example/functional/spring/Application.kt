e80542c09374cdffed5eda8dd193ba4c0e122dfe
doxcom <aldoperez1319@gmail.com>
2024-11-06 03:01:17 -0600

    first kotling fx

--- src/main/kotlin/com/example/functional/spring/Application.kt
+++ src/main/kotlin/com/example/functional/spring/Application.kt
@@ -2,10 +2,22 @@ package com.example.functional.spring
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
-class Application
+class Application{
+
+	fun process(s: String, callback: (String) -> Number){
+		println(callback.invoke("hello"))
+	}
+
+	init{
+       this.process("a") { 42 }
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<Application>(*args)
 }
+
+
